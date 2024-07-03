6dfdf5d48653e45605776f4a91a68433215a18a7
Igor Bartmann <ibartmann@gvdasa.com.br>
2024-07-02 21:56:24 -0300

    código perdido em resolução de conflito

--- GB_TrabalhoFinal/Exericio/Origem.cpp
+++ GB_TrabalhoFinal/Exericio/Origem.cpp
@@ -439,8 +439,7 @@ void setMaterialProperties(Shader& shader, const Material& material, bool isSele
 }
 
 // Função para atualizar os valores das matrizes modelo e projeção do objeto para movimentação.
-void update_object_matrix_to_move(int object_id, glm::mat4& model, glm::mat4& projection, float& zoom,
-								  float window_width, float window_height) {
+void update_object_matrix_to_move(int object_id, glm::mat4& model, glm::mat4& projection, float& zoom, float window_width, float window_height) {
 	if (object_id != selected_object_id) {
 		return;
 	}
@@ -483,9 +482,9 @@ void update_object_matrix_to_move(int object_id, glm::mat4& model, glm::mat4& pr
 }
 
 // Função para renderizar o objeto.
-void handle_object_render(Shader& shader, Mesh object, glm::mat4& model, glm::mat4& projection, float& zoom, GLuint texture_id, Material material) {
+void handle_object_render(Shader& shader, Mesh object, glm::mat4& model, glm::mat4& projection, float& zoom, GLuint texture_id, Material material, float window_width, float window_height) {
 	// Atualização das matrizes de modelo e projeção.
-	update_object_matrix_to_move(object.getId(), model, projection, zoom);
+	update_object_matrix_to_move(object.getId(), model, projection, zoom, window_width, window_height);
 	shader.setMat4("model", glm::value_ptr(model));
 	shader.setMat4("projection", glm::value_ptr(projection));
 
@@ -654,10 +653,10 @@ int main() {
 
 	// Definir a malha dos objetos.
 	Mesh obj1_mesh, obj2_mesh, obj3_mesh, obj4_mesh;
-	obj1_mesh.initialize(VAO1, nVertsObj1, &shader, obj1_position, obj1_scale, obj1_config.lookup("rotation"));
-	obj2_mesh.initialize(VAO2, nVertsObj2, &shader, obj2_position, obj2_scale, obj2_config.lookup("rotation"));
-	obj3_mesh.initialize(VAO3, nVertsObj3, &shader, obj3_position, obj3_scale, obj3_config.lookup("rotation"));
-	obj4_mesh.initialize(VAO4, nVertsObj4, &shader, obj4_position, obj4_scale, obj4_config.lookup("rotation"));
+	obj1_mesh.initialize(1, VAO1, nVertsObj1, &shader, obj1_position, obj1_scale, obj1_config.lookup("rotation"));
+	obj2_mesh.initialize(2, VAO2, nVertsObj2, &shader, obj2_position, obj2_scale, obj2_config.lookup("rotation"));
+	obj3_mesh.initialize(3, VAO3, nVertsObj3, &shader, obj3_position, obj3_scale, obj3_config.lookup("rotation"));
+	obj4_mesh.initialize(4, VAO4, nVertsObj4, &shader, obj4_position, obj4_scale, obj4_config.lookup("rotation"));
 
 	// Definiar material dos objetos
 	Material obj1_material = parseMTL(getMTLFilePath(obj1_config.lookup("obj_path")));
@@ -710,21 +709,17 @@ int main() {
 		shader.setVec3("cameraPos", cameraPosition.x, cameraPosition.y, cameraPosition.z);
 
 		// Renderização do Objeto 1.
-		handle_object_render(shader, obj1_mesh, obj1_model, obj1_projection, obj1_zoom, obj1_texID,
-							 obj1_material);
+		handle_object_render(shader, obj1_mesh, obj1_model, obj1_projection, obj1_zoom, obj1_texID, obj1_material);
 		
 		// Renderização do Objeto 2.
-		handle_object_render(shader, obj2_mesh, obj2_model, obj2_projection, obj2_zoom, obj2_texID,
-							 obj2_material);	
+		handle_object_render(shader, obj2_mesh, obj2_model, obj2_projection, obj2_zoom, obj2_texID, obj2_material);	
 
 		// Renderização do Objeto 3.
-		handle_object_render(shader, obj3_mesh, obj3_model, obj3_projection, obj3_zoom, obj3_texID,
-							 obj3_material);
+		handle_object_render(shader, obj3_mesh, obj3_model, obj3_projection, obj3_zoom, obj3_texID, obj3_material);
 
 		// Renderização do Objeto 4.
 		// Definição do material da superfície (textura).
-		update_object_matrix_to_move(4, obj4_model, obj4_projection, obj4_zoom, (float)window_width,
-									 (float)window_height);
+		update_object_matrix_to_move(4, obj4_model, obj4_projection, obj4_zoom, (float)window_width, (float)window_height);
 		shader.setMat4("model", glm::value_ptr(obj4_model));
 		shader.setMat4("projection", glm::value_ptr(obj4_projection));
 
@@ -751,15 +746,13 @@ int main() {
 		planetTranslation.y = 3.0f;
 
 		// Criando matrizes de Rotação e Transalação.
-		glm::mat4 rotation =
-			glm::rotate(glm::mat4(1.0f), glm::radians(planetRotationAngle), glm::vec3(0.0f, 1.0f, 0.0f));
+		glm::mat4 rotation = glm::rotate(glm::mat4(1.0f), glm::radians(planetRotationAngle), glm::vec3(0.0f, 1.0f, 0.0f));
 		glm::mat4 translation = glm::translate(glm::mat4(1.0f), planetTranslation);
 
 		// Aplicando a transformação a partir da translação e rotação.
 		glm::mat4 planetTransform = translation * rotation;
 		float modelArray[16];
-		memcpy(modelArray, glm::value_ptr(planetTransform * glm::scale(glm::mat4(1.0f), obj4_scale)),
-			   sizeof(float) * 16);
+		memcpy(modelArray, glm::value_ptr(planetTransform * glm::scale(glm::mat4(1.0f), obj4_scale)), sizeof(float) * 16);
 		shader.setMat4("model", modelArray);
 		shader.setMat4("projection", glm::value_ptr(obj4_projection));
 
